3e850a2586841d51a5663e7a6ba4bd4222be7f2b
Ching Jui Young <young.chingjui@gmail.com>
2024-04-18 17:16:50 +0800

    can edit api key

--- src/components/ApiKeyManager.jsx
+++ src/components/ApiKeyManager.jsx
@@ -1,9 +1,16 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 
 const ApiKeyManager = ({ apiKey, setApiKey }) => {
-    const saveAPIKey = () => {
-        localStorage.setItem("apiKey", apiKey)
+    const [isEditing, setIsEditing] = useState(false)
+
+    const handleEdit = () => {
+        if (isEditing) {
+            localStorage.setItem("apiKey", apiKey)
+            setIsEditing(false)
+        } else {
+            setIsEditing(true)
+        }
     }
 
     return (
@@ -14,21 +21,31 @@ const ApiKeyManager = ({ apiKey, setApiKey }) => {
             >
                 OpenAI API Key
             </label>
-            <input
-                type="text"
-                id="api-key-input"
-                value={apiKey}
-                onChange={(event) => setApiKey(event.target.value)}
-                className="w-full px-4 py-2 2 mb-2 border border-gray-300 rounded"
-                placeholder="Paste your API Key here"
-            />
-            <button
-                id="api-key-button"
-                className="px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-400 transition-colors"
-                onClick={saveAPIKey}
-            >
-                Save API Key
-            </button>
+            <div className="flex justify-between items-center w-full gap-2">
+                <input
+                    type="text"
+                    id="api-key-input"
+                    value={apiKey}
+                    onChange={(event) => setApiKey(event.target.value)}
+                    className={`w-full px-4 py-2 2 border border-gray-300 rounded ${
+                        !isEditing &&
+                        "cursor-not-allowed bg-gray-200 text-gray-400"
+                    }`}
+                    placeholder="Paste your OpenAI API key here"
+                    disabled={!isEditing}
+                />
+                <button
+                    onClick={handleEdit}
+                    id="edit-api-key-button"
+                    className={`px-4 py-2 min-w-24 border rounded transition-colors self-end ${
+                        isEditing
+                            ? "bg-indigo-500 text-white hover:bg-indigo-400 "
+                            : "border-gray-500 text-gray-500 hover:bg-gray-100"
+                    }`}
+                >
+                    {isEditing ? "Save" : "Edit"}
+                </button>
+            </div>
         </div>
     )
 }
